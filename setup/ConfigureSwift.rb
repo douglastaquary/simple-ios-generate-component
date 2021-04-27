2d102f3f12819fae40203549de31009a700961ae
Douglas Taquary <douglastaquary@gmail.com>
2021-04-26 21:33:33 -0300

    Fix issues.

--- setup/ConfigureSwift.rb
+++ setup/ConfigureSwift.rb
@@ -50,11 +50,11 @@ module Pod
 
       # There has to be a single file in the Classes dir
       # or a framework won't be created
-      `touch #{configurator.pod_name}/Classes/ReplaceMe.swift`
+      `touch Pod/Classes/ReplaceMe.swift`
 
       `mv ./templates/swift/* ./`
 
-      `mv ./Pod/* ./#{configurator.pod_name}`
+      `mv Pod ./#{configurator.pod_name}`
 
       #{pod_name}
 
